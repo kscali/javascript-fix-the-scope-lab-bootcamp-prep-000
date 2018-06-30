2de423c7abb2d45c53e5e7260979567ec3d75a79
kosatun@aol.com <kosatun@aol.com>
2018-06-30 01:41:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var funkyFunction = function() {
       return "FUNKY!";
       };
 };
-funk();
+
 var theFunk = funkyFunction();
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
